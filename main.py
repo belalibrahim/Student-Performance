8a082da3dcaa8b1e4e84b8b996c5ed28dd747e1a
Belal <billy.fcih@yahoo.com>
2017-11-24 19:00:37 +0200

    Initialize the project

---
+++ main.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
+import seaborn as sns
+import pandas as pd
+import numpy as np
+import scipy as sc
+import random
+
+students_math = pd.read_csv("dataset/student/student-mat.csv")
+
+print(students_math)
