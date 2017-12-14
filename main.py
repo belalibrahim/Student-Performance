4759b38950a9997318c1795f6a74509423180371
Belal <billy.fcih@yahoo.com>
2017-12-15 00:03:48 +0200

    Add basic plots

--- main.py
+++ main.py
@@ -6,6 +6,38 @@ import numpy as np
 import scipy as sc
 import random
 
-students_math = pd.read_csv("dataset/student/student-mat.csv")
+students_math = pd.read_csv("dataset/student/student-mat.csv", sep=';')
 
-print(students_math)
+# Rug plot
+sns.rugplot(students_math['studytime'])
+# Scatter Plot
+plt.scatter('studytime', 'age', data=students_math)
+students_math.plot(x="studytime", y="age", kind="scatter")
+sns.jointplot(x="studytime", y="age", data=students_math, size=5)
+sns.FacetGrid(students_math, hue="school", size=5).map(plt.scatter, "studytime", "age").add_legend()
+# Histogram Plot
+plt.hist('studytime', data=students_math)
+sns.distplot(students_math['studytime'])
+students_math.plot(x="studytime", kind="hist")
+# Box plot
+sns.boxplot(data=students_math)
+
+# information about ages
+students_math["age"].value_counts()
+
+# Statistical description for data
+students_math.describe()
+students_mean = students_math.mean()
+students_std = students_math.std()
+
+# Generate 50 random samples
+for i in range(0, 50):
+    # 10 observation per sample
+    sample = students_math.sample(n=10)
+    sample_mean = sample.mean()
+    sample_std = sample.std()
+
+# Correlation
+students_math.corr()
+
+sns.pairplot(students_math, hue="school", size=3, kind='scatter', diag_kind='kde').add_legend()
