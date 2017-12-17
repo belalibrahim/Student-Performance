a58119eda80fcc352f2d1fd569b5f18006620c4d
Belal <billy.fcih@yahoo.com>
2017-12-17 10:06:02 +0200

    Add more plots

--- main.py
+++ main.py
@@ -14,29 +14,38 @@ sns.stripplot(x="paid", y="failures", data=students_math, jitter=True)
 sns.stripplot(x="nursery", y="G3", data=students_math, jitter=True)
 sns.stripplot(x="internet", y="G3", data=students_math, jitter=True)
 sns.stripplot(x="internet", y="absences", data=students_math, jitter=True)
+sns.stripplot(x="higher", y="G3", data=students_math, jitter=True)
+sns.stripplot(x="famrel", y="G3", data=students_math, jitter=True)
 
 # Scatter Plot
-#sns.jointplot(x="Medu", y="G3", data=students_math)
 sns.jointplot(x="G2", y="G3", data=students_math)
 sns.jointplot(x="failures", y="G3", data=students_math)
 sns.jointplot(x="absences", y="G3", data=students_math)
 
 sns.jointplot(x="age", y="G3", data=students_math)
+sns.jointplot(x="Medu", y="G3", data=students_math)
 sns.jointplot(x="traveltime", y="G3", data=students_math)
-sns.jointplot(x="failures", y="absences", data=students_math)
 sns.jointplot(x="studytime", y="G3", data=students_math)
 
 # Histogram Plot
-plt.hist(x='G3', data=students_math)
+sns.distplot(students_math['G3'])
+sns.distplot(students_math['age'])
+sns.distplot(students_math['studytime'])
+sns.distplot(students_math['traveltime'])
+sns.distplot(students_math['absences'])
+sns.distplot(students_math["health"])
+sns.distplot(students_math["goout"])
 plt.hist2d(x="G2", y="G3", data=students_math)
 plt.hist2d(x="failures", y="G3", data=students_math)
-# sns.distplot(students_math['studytime'])
 # Box plot
 sns.boxplot(x="paid", y="G3", data=students_math)
 sns.boxplot(x="paid", y="studytime", data=students_math)
+sns.boxplot(x="sex", y="G3", data=students_math)
+sns.boxplot(x="schoolsup", y="G3", data=students_math)
 sns.boxplot(x="internet", y="G3", data=students_math)
 sns.boxplot(x="guardian", y="G3", data=students_math)
-sns.boxplot(data=students_math)
+sns.boxplot(x="activities", y="G3", data=students_math)
+sns.boxplot(x="freetime", y="G3", data=students_math)
 
 # information about ages
 students_math["age"].value_counts()
@@ -50,7 +59,7 @@ students_std = students_math.std()
 total_sample_mean = {}
 total_sample_std = {}
 # Generate 50 random samples
-for i in range(0, 50):
+for i in range(50):
     # 10 observation per sample
 
     sample = students_math.sample(n=10)
@@ -67,7 +76,7 @@ for i in range(0, 50):
 
 for key in total_sample_mean:
     total_sample_mean[key] /= 50
-    total_sample_std[key] /= 50
+    total_sample_std[key] /= 49
 
 
 # Correlation
